35161b0887bc16a99b5296ecc9804ffa5faeaa22
M Lintang Ashshofa W <97678885+lintangashsh@users.noreply.github.com>
2023-11-11 01:15:17 +0700

    havivya

---
+++ application/views/auth/blocked.php
@@ -0,0 +1,5 @@
+<div class="text-center pt-24 bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+    <strong class="font-bold">Error 403 - Forbidden</strong>
+    <br>
+    You don't have an access to this section!
+</div>
---
+++ application/views/user/tentang.php
@@ -0,0 +1,46 @@
+<body class="w-full h-full" id="up">
+    <div class="px-10 lg:px-20 pt-24">
+        <h1 class="font-montserrat my-10 lg:my-20 text-xl lg:text-4xl text-center font-bold">Tentang Rumah Kue Havivya</h1>
+        <div class="lg:px-48 lg:mt-10 mt-5">
+            <div class="w-full h-full px-4 shadow-md">
+                <img src=" <?= base_url('assets/img/logo.png') ?> " class="h-36 mx-auto" data-aos="zoom-in">
+
+                <h1 class="font-montserrat font-bold text-sm lg:text-base">Rumah Kue Havivya</h1>
+
+                <p class="lg:mt-5 mt-2 leading-8  font-montserrat font-light text-sm text-justify lg:text-base">
+                    Rumah Kue Havivya merupakan sebuah bisnis kecil keluarga yang telah kami bangun sejak Februari 2022. Rumah Kue Havivya awalnya hanya sebuah usaha pembuatan kue rumahan biasa yang hanya menjalani serta memproduksi kue untuk lebaran saja, namun seiring berjalannya waktu, kami memutuskan untuk mengembangkan produksi yang kami buat menjadi memproduksi kue-kue sebagai sebuah jajanan atau cemilan kesukaan anda ketika di rumah atau pun dimana pun anda berada.
+                </p>
+
+                <br>
+
+                <p class="leading-loose font-montserrat font-light text-sm text-justify lg:text-base">
+                    Beberapa produk kue yang telah kami buat adalah kue dengan jenis Brownies, kue kering atau cookies, pie, serta bolu biasa. Saat ini, kami telah memproduksi pula kue-kue ulang tahun yang akan diproduksi sesuai request anda sebagai pelanggan terhormat kami. Namun, untuk kue ulang tahun, anda harus memesannya melalui private chat kami di whatsapp yang telah tertera pada web ini atau anda dapat mengunjungi langsung tempat pembuatan serta toko offline dari Rumah Kue Havivya ini menggunakan panduan google maps yang ada pada website ini. Beberapa gambaran produk yang telah kami produki yang ada pada tampilan website ini merupakan beberapa produk kami yang sering dipesan serta dibeli oleh beberapa pelanggan setia kami.
+                </p>
+
+                <br>
+
+                <p class="leading-loose font-montserrat font-light text-sm text-justify lg:text-base">
+                    Kami harap, kami dapat menjadi pilihan anda sebagai pelanggan kami dalam memilih toko penjualan kue-kue kering serta basah dikemudian hari. Jika anda puas, harap kabari teman atau rekan kerja anda. Tapi, kalau anda tidak puas, harap kabari kami secepatnya.
+                </p>
+                <br>
+
+                <div class="grid grid-cols-12 grid-flow-row gap-1 w-full py-3">
+                    <div class="justify-self-start w-auto col-span-2 lg:col-span-1">
+                        <i class="fa-solid fa-fw fa-location-dot text-2xl items-center"></i>
+                    </div>
+                    <div class="font-montserrat font-light text-sm lg:text-base text-left lg:col-span-11 col-span-10">
+                        Jl. Ampera I No.28a, Sei Sikambing C. II, Kec. Medan Helvetia, Kota Medan, Sumatera Utara 20123
+                    </div>
+                    <div class="justify-self-start col-span-2 lg:col-span-1">
+                        <i class="fa-brands fa-fw fa-whatsapp text-2xl"></i>
+                    </div>
+                    <div class="font-montserrat font-light text-sm lg:text-base text-left lg:col-span-11 col-span-10">
+                        +6282168677888
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+    </div>
+</body>
